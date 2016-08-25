c0d89df237b37d112921f210bfbc7f36cefc8862
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-08-25 22:53:43 +0200

    Add base files

---
+++ config/common.mk
@@ -0,0 +1,7 @@
+# Additional packages
+# -include vendor/dk/config/packages.mk
+
+# PRODUCT_PACKAGE_OVERLAYS += vendor/dk/overlay
+
+# Versionning
+# -include vendor/dk/config/version.mk
---
+++ config/version.mk
@@ -0,0 +1,1 @@
+# Versioning of the ROM
