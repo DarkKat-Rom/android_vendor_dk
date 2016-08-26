d3b27fd88a091aae678aec953b30f8b143a6e6f4
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-08-26 02:52:54 +0200

    DarkKat N

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,32 @@
-# Additional packages
-# -include vendor/dk/config/packages.mk
+# Sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Playa.ogg \
+    ro.config.notification_sound=regulus.ogg \
+    ro.config.alarm_alert=Alarm_Beep_03.ogg
 
-# PRODUCT_PACKAGE_OVERLAYS += vendor/dk/overlay
+# Selinux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/dk/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/dk/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/dk/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+    vendor/dk/prebuilt/bin/blacklist:system/addon.d/blacklist
+
+# init.d support
+#PRODUCT_COPY_FILES += \
+#    vendor/dk/prebuilt/bin/sysinit:system/bin/sysinit
+
+# Init script file with DarkKat extras
+#PRODUCT_COPY_FILES += \
+#    vendor/dk/prebuilt/etc/init.local.rc:root/init.dk.rc
+
+# Additional packages
+#-include vendor/dk/config/packages.mk
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/dk/overlay
 
 # Versionning
-# -include vendor/dk/config/version.mk
+-include vendor/dk/config/version.mk
--- config/version.mk
+++ config/version.mk
@@ -1,1 +1,7 @@
 # Versioning of the ROM
+ ROM_VERSION := $(BUILD_ID)-$(shell date +%Y%m%d)-$(DK_BUILD_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
+ DK_BUILD_VERSION := $(BUILD_ID)-$(shell date +%Y%m%d)-$(subst dk_,,$(TARGET_PRODUCT))
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.dk.build.version=$(DK_BUILD_VERSION) \
+    ro.dk.build.type=$(DK_BUILD_TYPE)
