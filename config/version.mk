e659636125c38a820c28887b3e5017422e921289
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-09-05 22:39:28 +0200

    Darkkat system properties: Clean up, (2/2)

--- config/version.mk
+++ config/version.mk
@@ -1,10 +1,11 @@
-# Versioning of the ROM
- DK_BUILD_VERSION_DISPLAY := $(BUILD_ID)-$(shell date +%Y%m%d)
- DK_BUILD_VERSION := $(DK_BUILD_VERSION_DISPLAY)-$(subst dk_,,$(TARGET_PRODUCT))
- ROM_VERSION := $(DK_BUILD_VERSION_DISPLAY)-$(DK_BUILD_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
+# Used to display the version
+DK_VERSION := N
+# Used to display the build version
+DK_BUILD_VERSION := $(BUILD_ID)-$(shell date +%Y%m%d)
+# Used for the rom zip name
+ROM_VERSION := $(DK_BUILD_VERSION)-$(DK_BUILD_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.dk.version=N \
-    ro.dk.build.version.display=$(DK_BUILD_VERSION_DISPLAY) \
+    ro.dk.version=$(DK_VERSION) \
     ro.dk.build.version=$(DK_BUILD_VERSION) \
     ro.dk.build.type=$(DK_BUILD_TYPE)
