282eb38ee605bde093fbaca57b801745c5f9477a
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-09-12 16:20:12 +0200

    Change target package name and update dk system properties, (3/3)

--- config/version.mk
+++ config/version.mk
@@ -1,11 +1,13 @@
 # Used to display the version
-DK_VERSION := N
+DK_VERSION_SHORT := N
+DK_VERSION := DarkKat-$(DK_VERSION_SHORT)
 # Used to display the build version
 DK_BUILD_VERSION := $(BUILD_ID)-$(shell date +%Y%m%d)
 # Used for the rom zip name
-ROM_VERSION := $(DK_BUILD_VERSION)-$(DK_BUILD_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
+DK_TARGET_PACKAGE_NAME := $(DK_VERSION)-$(shell date +%Y%m%d)-$(DK_BUILD_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.dk.version_short=$(DK_VERSION_SHORT) \
     ro.dk.version=$(DK_VERSION) \
     ro.dk.build.version=$(DK_BUILD_VERSION) \
     ro.dk.build.type=$(DK_BUILD_TYPE)
