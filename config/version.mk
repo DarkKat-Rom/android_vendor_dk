a8d38f89a5ba7f9843982868236ca30809811366
XXMrHyde <xxmrhyde@gmx.co.uk>
2017-06-05 00:25:41 +0200

    Update and clean up version

--- config/version.mk
+++ config/version.mk
@@ -1,13 +1,6 @@
 # Used to display the version
-DK_VERSION_SHORT := N-1
-DK_VERSION := DarkKat-$(DK_VERSION_SHORT)
+DK_VERSION := DarkKat-Pure-N-1.2
 # Used to display the build version
 DK_BUILD_VERSION := $(BUILD_ID)-$(shell date +%Y%m%d)
 # Used for the rom zip name
 DK_TARGET_PACKAGE_NAME := $(DK_VERSION)-$(shell date +%Y%m%d)-$(DK_BUILD_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.dk.version_short=$(DK_VERSION_SHORT) \
-    ro.dk.version=$(DK_VERSION) \
-    ro.dk.build.version=$(DK_BUILD_VERSION) \
-    ro.dk.build.type=$(DK_BUILD_TYPE)
