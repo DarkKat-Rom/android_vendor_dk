79cd8a473c09aa3e65d9b649c06334db439245f1
XXMrHyde <xxmrhyde@gmx.co.uk>
2017-10-10 23:47:11 +0200

    Import backuptool, (7.1 version)

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,11 @@
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/dk/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/dk/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/dk/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+vendor/dk/prebuilt/bin/blacklist:system/addon.d/blacklist
+
+# Add DarkKat  overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/dk/overlay
 
 # Versionning
