8cc935692c75e2c5a237403fb2f1761e8f9e5ce4
XXMrHyde <xxmrhyde@gmx.co.uk>
2018-01-01 22:53:44 +0100

    Theming: Switch to RRO overlays, (5/5):
    (1/5): android_frameworks_base
    (2/5): android_packages_apps_DKSettings
    (3/5): android_packages_apps_Terminal
    (4/5): android_packages_overlays
    (5/5): android_vendor_dk
    
    Build RRO overlay packages

--- config/packages.mk
+++ config/packages.mk
@@ -3,3 +3,6 @@ PRODUCT_PACKAGES += \
     DKSettings \
     DKWeather \
     Terminal
+
+# Additional theme overlay packages
+-include vendor/dk/config/theme_overlay_packages.mk
---
+++ config/theme_overlay_packages.mk
@@ -0,0 +1,39 @@
+# ShutdownThread dialog overlay themes
+PRODUCT_PACKAGES += \
+    ShutdownThreadBlackoutThemeOverlay \
+    ShutdownThreadDarkKatBlackThemeOverlay \
+    ShutdownThreadDarkKatLightThemeOverlay \
+    ShutdownThreadDarkKatThemeOverlay \
+    ShutdownThreadDarkKatWhiteOverlay \
+    ShutdownThreadDarkThemeOverlay \
+    ShutdownThreadWhiteoutThemeOverlay
+
+# SystemUI overlay themes
+PRODUCT_PACKAGES += \
+    SysuiBlackoutThemeOverlay \
+    SysuiDarkKatBlackThemeOverlay \
+    SysuiDarkKatLightThemeOverlay \
+    SysuiDarkKatThemeOverlay \
+    SysuiDarkKatWhiteOverlay \
+    SysuiDarkThemeOverlay \
+    SysuiWhiteoutThemeOverlay
+
+# DK Settings overlay themes
+PRODUCT_PACKAGES += \
+    DKSettingsBlackoutThemeOverlay \
+    DKSettingsDarkKatBlackThemeOverlay \
+    DKSettingsDarkKatLightThemeOverlay \
+    DKSettingsDarkKatThemeOverlay \
+    DKSettingsDarkKatWhiteOverlay \
+    DKSettingsDarkThemeOverlay \
+    DKSettingsWhiteoutThemeOverlay
+
+# Terminal overlay themes
+PRODUCT_PACKAGES += \
+    TerminalBlackoutThemeOverlay \
+    TerminalDarkKatBlackThemeOverlay \
+    TerminalDarkKatLightThemeOverlay \
+    TerminalDarkKatThemeOverlay \
+    TerminalDarkKatWhiteOverlay \
+    TerminalDarkThemeOverlay \
+    TerminalWhiteoutThemeOverlay
