70a5c297f81f0f45ec79bfc3dee92fd74693eea0
XXMrHyde <xxmrhyde@gmx.co.uk>
2018-08-30 20:08:33 +0200

    Add support for Android Go:
    NOTE:
    For further testing,
    Android GO target is removed in the flo device tree.

---
+++ config/common_go.mk
@@ -0,0 +1,21 @@
+# Sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Playa.ogg \
+    ro.config.notification_sound=regulus.ogg \
+    ro.config.alarm_alert=Alarm_Beep_03.ogg
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/dk/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/dk/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/dk/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+    vendor/dk/prebuilt/bin/blacklist:system/addon.d/blacklist
+
+# Additional packages
+-include vendor/dk/config/packages.mk
+
+# Add DarkKat  overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/dk/overlay
+
+# Versionning
+-include vendor/dk/config/version_go.mk
